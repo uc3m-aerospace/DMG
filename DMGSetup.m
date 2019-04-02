2563229666a17a16ed1bc1329ee894e7f65a67b8
DAVID MORANTE <dmorante@ing.uc3m.es>
2019-04-02 11:37:49 +0200

    Include license for GPOPS

--- DMGSetup.m
+++ DMGSetup.m
@@ -1,13 +1,14 @@
-
-% -------------------- %
-% Setup File for DMG %
-% -------------------- %
+% ------------------------ %
+% Setup File for DMG tool  %
+% ------------------------ %
 % Notes: 
 %   (1) This file assumes that you have write permissions
 %       to change the MATLAB path.  If you do cannot change the
 %       MATLAB path, contact your system administrator.
-%   (2) Precompiled SNOPT and IPOPT libraries are included in the DMG distribution.
-%   (3) Built-in automatic differentiation is included in the DMG
+%   (2) Precompiled IPOPT libraries are included in the DMG distribution.
+%   (3) Precompiled SNOPT libraries are not included in the DMG distribution, and 
+%       have to be obtained separately (see Readme File).
+%   (4) Built-in automatic differentiation is included in the DMG
 %       distribution and is installed by default using this setup
 %       file.
 
